1b4292216c8b343ed8d73203ed0e893ce51be55d
Linards Kalvāns <linards.kalvans@gmail.com>
2019-10-21 22:53:27 +0300

    Fix paths for rsync

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 chmod 600 deploy_key
-rsync -Pav -e "ssh -o StrictHostKeyChecking=no -i deploy_key" --exclude=deploy_key $DEPLOY_HOST:./ ~/shiny/appdir/bernudarzi
+rsync -Pav -e "ssh -o StrictHostKeyChecking=no -i deploy_key" --exclude=deploy_key ./ $DEPLOY_HOST:~/shiny/appdir/bernudarzi
